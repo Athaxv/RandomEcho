7885ae232179b6a6331ef067db41424f90848056
AtharvGaur <atharava112@gmail.com>
2024-12-29 15:50:20 +0530

    installed axios

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+"use client" 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
 
 export default function page() {
   return (
