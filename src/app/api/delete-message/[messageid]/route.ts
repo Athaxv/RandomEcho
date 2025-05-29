e505957dd9981b90caa7557319c8b82c8853d86e
AtharvGaur <atharava112@gmail.com>
2025-05-29 12:45:34 +0530

    Refactor DELETE API route to use NextResponse and improve type handling for user session

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -3,17 +3,20 @@ import { getServerSession } from 'next-auth/next';
 import dbConnect from '@/lib/dbConnect';
 import { User } from 'next-auth';
 import { authOptions } from '../../auth/[...nextauth]/options';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function DELETE(
-  request: Request,
+  req: NextRequest,
   { params }: { params: { messageid: string } }
 ) {
   const messageId = params.messageid;
   await dbConnect();
+
   const session = await getServerSession(authOptions);
-  const _user: User = session?.user;
+  const _user = session?.user as User & { _id: string };
+
   if (!session || !_user) {
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
@@ -26,19 +29,19 @@ export async function DELETE(
     );
 
     if (updateResult.modifiedCount === 0) {
-      return Response.json(
+      return NextResponse.json(
         { message: 'Message not found or already deleted', success: false },
         { status: 404 }
       );
     }
 
-    return Response.json(
+    return NextResponse.json(
       { message: 'Message deleted', success: true },
       { status: 200 }
     );
   } catch (error) {
     console.error('Error deleting message:', error);
-    return Response.json(
+    return NextResponse.json(
       { message: 'Error deleting message', success: false },
       { status: 500 }
     );
